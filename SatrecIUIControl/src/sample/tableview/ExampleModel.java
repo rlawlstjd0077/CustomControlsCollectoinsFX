025dfe69733d22d6a9ff96f2763307da811e068f
김진성 <rlawlstjd077@gmail.com>
2017-01-12 16:51:20 +0900

    add tableview

---
+++ SatrecIUIControl/src/sample/tableview/ExampleModel.java
@@ -0,0 +1,41 @@
+package sample.tableview;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * 테스트에 쓰일 TableView의 모델
+ * Created by 신동찬
+ */
+public class ExampleModel {
+  private SimpleStringProperty first;
+  private SimpleStringProperty second;
+
+  public ExampleModel(String first, String second) {
+    this.first = new SimpleStringProperty(first);
+    this.second = new SimpleStringProperty(second);
+  }
+
+  public String getFirst() {
+    return first.get();
+  }
+
+  public void setFirst(String first) {
+    this.first.set(first);
+  }
+
+  public SimpleStringProperty firstProperty() {
+    return first;
+  }
+
+  public String getSecond() {
+    return second.get();
+  }
+
+  public void setSecond(String second) {
+    this.second.set(second);
+  }
+
+  public SimpleStringProperty secondProperty() {
+    return second;
+  }
+}
