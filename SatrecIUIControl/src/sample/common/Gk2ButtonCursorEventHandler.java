c34a8b51b2998fddf4cba10aaf25703a47bb839f
김진성 <rlawlstjd077@gmail.com>
2017-02-07 14:56:03 +0900

    add button cursor event handler

---
+++ SatrecIUIControl/src/sample/common/Gk2ButtonCursorEventHandler.java
@@ -0,0 +1,22 @@
+package sample.common;
+
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.control.ButtonBase;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * Created by GSD on 2017-02-07.
+ */
+public class Gk2ButtonCursorEventHandler implements EventHandler<MouseEvent> {
+    @Override
+    public void handle( final MouseEvent ME )
+    {
+        Object obj = ME.getSource();
+        /**
+         * 모든 버튼을 포함하는 상위 클래스인 ButtonBase를 사용
+         */
+        ButtonBase button = (ButtonBase) obj;
+        button.setCursor(Cursor.HAND);
+    }
+}
