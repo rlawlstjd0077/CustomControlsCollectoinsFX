a4bd089947f232c6ee7097bc51da739e57a9a718
김진성 <rlawlstjd077@gmail.com>
2017-02-27 17:01:41 +0900

    add LAE Burn Plan screen

---
+++ CustomControlFX/src/main/java/ui/common/DataSearchTableRowViewModel.java
@@ -0,0 +1,32 @@
+package ui.common;
+
+import javafx.beans.property.StringProperty;
+
+/**
+ * Created by GSD on 2017-02-27.
+ */
+public class DataSearchTableRowViewModel {
+  private StringProperty name;
+  private StringProperty Date;
+
+  public DataSearchTableRowViewModel(StringProperty name, StringProperty date) {
+    this.name = name;
+    Date = date;
+  }
+
+  public StringProperty nameProperty() {
+    return name;
+  }
+
+  public StringProperty dateProperty() {
+    return Date;
+  }
+
+  public String getName() {
+    return name.get();
+  }
+
+  public String getDate() {
+    return Date.get();
+  }
+}
---
+++ CustomControlFX/src/main/java/ui/screen/LAEburnplangeneration/LAEBurnPlanGenerationViewModel.java
@@ -0,0 +1,20 @@
+package ui.screen.LAEburnplangeneration;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import ui.common.DataSearchTableRowViewModel;
+
+/**
+ * Created by GSD on 2017-02-23.
+ */
+public class LAEBurnPlanGenerationViewModel {
+  ObservableList<DataSearchTableRowViewModel> dataSearchTableRowViewModelList = FXCollections.observableArrayList(
+    new DataSearchTableRowViewModel(new SimpleStringProperty("FOCUSLEOP_DATA_1"), new SimpleStringProperty("2015-01-01 00:00:00")),
+    new DataSearchTableRowViewModel(new SimpleStringProperty("FOCUSLEOP_DATA_2"), new SimpleStringProperty("2015-01-01 00:00:00")),
+    new DataSearchTableRowViewModel(new SimpleStringProperty("FOCUSLEOP_DATA_3"), new SimpleStringProperty("2015-01-01 00:00:00")),
+    new DataSearchTableRowViewModel(new SimpleStringProperty("FOCUSLEOP_DATA_4"), new SimpleStringProperty("2015-01-01 00:00:00")),
+    new DataSearchTableRowViewModel(new SimpleStringProperty("FOCUSLEOP_DATA_5"), new SimpleStringProperty("2015-01-01 00:00:00")),
+    new DataSearchTableRowViewModel(new SimpleStringProperty("FOCUSLEOP_DATA_6"), new SimpleStringProperty("2015-01-01 00:00:00"))
+  );
+}
